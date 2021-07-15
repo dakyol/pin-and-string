4da5eb0d4692e3f716952d6325d1d3934f8b042a
Deniz Windows <nad.ozgurdeniz@gmail.com>
2021-07-15 16:18:52 +0300

    Add new classes

--- src/pin-and-string/FociMatrix.py
+++ src/pin-and-string/FociMatrix.py
@@ -1,19 +1,22 @@
 import numpy as np
+from numpy.core.numeric import outer
 
 def H(Matrix, R):
     return("")
 
 class FociMatrix():
-    def __init__(self, x_function, y_function, domain):
-        self.matrix = np.array([x_function(domain),y_function(domain)])
+    #def __init__(self, x_function, y_function, domain):
+    #    self.matrix = np.array([x_function(domain),y_function(domain)])
     
-    def is_clockwise(self):
+    def __init__(self,Matrix):
+        self.matrix = Matrix
+
+    def is_clockwise(M):
         summa = 0
-        M = self.matrix
         [row, column] = M.shape
         for i in range(row-1):
-            summa += (M[0][i+1]-M[0][i])*(M[1][i+1]+M[1][i])
-        summa += (M[0][0]-M[0][-1])*(M[1][0]+M[1][-1])
+            summa += (M[i+1,0]-M[i,0])*(M[i+1,1]+M[i,1])
+        summa += (M[0,0]-M[-1,0])*(M[0,1]+M[-1,1])
         if summa == 0:
             raise Exception("This closed surface is not two dimensional!")
         elif summa > 0:
@@ -22,8 +25,8 @@ class FociMatrix():
             result = False
         return(result)
 
-    def is_counterclockwise(self):
-        result = not self.is_clockwise()
+    def is_counterclockwise(M):
+        result = not FociMatrix.is_clockwise(M)
         return(result)
 
     def convex_is_clockwise(self):
@@ -34,30 +37,174 @@ class FociMatrix():
             matrix = np.array([[M[0][0],M[0][1],M[0][2]],[M[1][0],M[1][1],M[1][2]]])
         return(self.is_clockwise())
 
-    def make_clockwise(self):
+    def make_clockwise(self):#Bu aslında çok zor sadece counterclockwise ise çeviriyor şu an
         if self.matrix.is_counterclockwise():
             self.matrix = np.flip(self.matrix)
             
     def make_counterclockwise(self):
         if self.matrix.is_clockwise():
             self.matrix = np.flip(self.matrix)
+        
+    def arc_length(self,n1,n2):
+        M = self.matrix
+        indices1 = range(n1,n2)
+        indices2 = range(n1+1,n2+1)
+        D = M.take(indices1, axis=0, mode='wrap')-M.take(indices2, axis=0, mode='wrap')
+        S = np.linalg.norm(np.transpose(D),axis=0)
+        leng = np.sum(S)
+        return(leng)
 
 class FiniteFociMatrix(FociMatrix):
 
-    def paired_foci(self, R):
-        [row, column] = self.matrix.shape
-        for i in range(row):
-            
-        M = ""
-        N = M
-        if N.convex_is_clockwise():
-            n = 1
+    def is_pair(self,R,n1,n2):#self alınca aşaığda kullanacaksak her seferinde matrixi tekrar çağırmaya çalışacağız
+        if n1 == n2:
+            return False
+
+        leng = self.arc_length(n1,n2)
+
+        M = self.matrix
+        [row, column] = M.shape
+
+        x1 = M.take(n1, axis=0, mode='wrap')
+        x1_next = M.take(n1+1, axis=0, mode='wrap')
+        
+        x2 = M.take(n2, axis=0, mode='wrap')
+        x2_prev = M.take(n2-1, axis=0, mode='wrap')
+
+        vec_x1 = x1_next - x1
+        vec_x2 = x2_prev - x2
+
+        a = vec_x2[0]
+        b = -vec_x1[0]
+
+        c = vec_x2[1]
+        d = -vec_x1[1]
+
+        M = np.array([[a,b],[c,d]])
+
+        try:
+            [alpha, beta] = np.linalg.solve(M,x1-x2)
+            y = x1 + alpha*vec_x1
+        except np.linalg.LinAlgError as err:
+            if 'Singular matrix' in str(err):
+                return False #İçten gelenler paralel yapıyor yani olmaz bunlar
+            else:
+                return("Unknown error occured!")
+
+        if (n1+1-n2)%row == 0:
+            cond = True
         else:
-            n = 1
-        return(M)
+            cond = FociMatrix.is_counterclockwise(np.array([x1,y,x2]))
+
+        if cond: #FociMatrix.is_counterclockwise(np.array([x1,y,x2])):
+            inner_perimeter = leng + np.abs(alpha)+np.abs(beta)
+            inner_condition = R > inner_perimeter
+            if inner_condition:
+                x1_prev = M.take(n1-1, axis=0, mode='wrap')
+                x2_next = M.take(n2+1, axis=0, mode='wrap')
+                
+                vec_x1 = x1 - x1_prev
+                vec_x2 = x2 - x2_next
+
+                a = vec_x2[0]
+                b = -vec_x1[0]
+
+                c = vec_x2[1]
+                d = -vec_x1[1]
+
+                M = np.array([[a,b],[c,d]])
+
+                try:
+                    [alpha, beta] = np.linalg.solve(M,x1-x2)
+                    y = x1 + alpha*vec_x1
+                except np.linalg.LinAlgError as err:
+                    if 'Singular matrix' in str(err):
+                        return True #Aynı olmadığını biliyoruz en başta eledik, o zaman paraleller
+                    else:
+                        return("Unknown error occured!")
+
+                if (n1-1-n2)%row == 0:
+                    cond = True
+                else:
+                    cond = FociMatrix.is_counterclockwise(np.array([x1,y,x2]))
+
+                if cond: #FociMatrix.is_counterclockwise(np.array([x1,y,x2])):
+                    if (n1-1-n2)%row == 0:
+                        return True #Dışarıdan aldığımız dğrusalsa o zaman bunlar uçtadır yani olur ilki yani içten alınan zaten sağlanyıor
+                    else:
+                        outer_perimeter = leng + np.abs(alpha)+np.abs(beta)
+                        outer_condition = R < outer_perimeter
+                        if outer_condition:
+                            return True
+                        else:
+                            return False #Dışarıdan alınan counterclockwise ise R'den küçük olmalı
+                else:
+                    return True
+            else:
+                return False #İçeriden alınandan büyük olmalı R
+        else:
+            return False #they are not pair
+        #############################################
+        x1_prev = np.take(M,n1-1)
+        x2_next = np.take(M,n2+1)
+
+        vec_x1 = x1 - x1_prev
+        vec_x2 = x2 - x2_next
+
+        a = vec_x2[0]
+        b = -vec_x1[0]
+
+        c = vec_x2[1]
+        d = -vec_x1[1]
+
+        M = np.array([[a,b],[c,d]])
+
+        [alpha, beta] = np.linalg.solve(M,x1-x2)
+
+        y = x1 + alpha*vec_x1
+
+        if FociMatrix.convex_is_clockwise(np.array([x1,y,x2])):
+            outer_perimeter = leng + np.abs(alpha)+np.abs(beta)
+            outer_condition = R < outer_perimeter
+            if outer_condition:
+                return True
+        else:
+            outer_condition = True
+            return True
+
+        if inner_condition and outer_condition:
+            return True
+
+    def paired_foci(self, R): #Karmaşıklığı 3n*O("is_pair()") arttırıyor (+3n*k)
+        M = self.matrix
+        [row, column] = M.shape
+        pair_list = -np.ones([row*row,column]) #preallocation
+
+        index = range(row)
+
+        for i in index:
+            if self.is_pair(R,0,i):#FiniteFociMatrix.is_pair(M,R,0,i)
+                pair_list[0,:] = [0,i]
+                righto = i
+
+        #her ikisi de false ise kesin 1 righto+1 eş
+        lefto = 0
+        index = 1
+        while lefto < row-1:#bilmem olabilir
+            if self.is_pair(R,lefto+1,righto): #FiniteFociMatrix.is_pair(M,R,lefto+1,righto):
+                lefto += 1
+            elif self.is_pair(R,lefto,righto+1): #FiniteFociMatrix.is_pair(M,R,lefto,righto+1):
+                righto += 1
+            else:#FiniteFociMatrix.is_pair(M,R,lefto+1,righto+1)
+                lefto += 1
+                righto += 1
+            pair_list[index,:]=[lefto,righto]
+            index += 1
+        
+        return(pair_list)
 
     def transform(self, R):
-        self.transformed_matrix = HTransformedMatrix(self, self.matrix, R)
+        self.transformed_matrix = self.HTransformedMatrix(self.matrix, R)
         return(H(self.matrix,R))
 
 class HTransformedMatrix():
